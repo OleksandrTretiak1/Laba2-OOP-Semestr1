f60d3db49b693a8cd375a743cd9ae551c4f13cd6
OleksandrTretiak1 <ssanka467@gmail.com>
2024-10-27 19:42:16 +0200

    Створюю новий проект для 2 блоку/завдання

---
+++ Лабораторна робота 2 ООП Завдання 2 (1 семестр)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_робота_2_ООП_Завдання_2__1_семестр_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
