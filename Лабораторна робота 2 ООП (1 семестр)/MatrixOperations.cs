240b615c45d29e2165bf425e43b24b6c83ed10a8
OleksandrTretiak1 <ssanka467@gmail.com>
2024-10-27 19:37:24 +0200

    Другий файл готовий

--- Лабораторна робота 2 ООП (1 семестр)/MatrixOperations.cs
+++ Лабораторна робота 2 ООП (1 семестр)/MatrixOperations.cs
@@ -4,9 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Лабораторна_робота_2_ООП__1_семестр_
+namespace MatrixNamespace
 {
-    internal class MatrixOperations
+    public partial class MyMatrix
     {
+        public static MyMatrix operator +(MyMatrix a, MyMatrix b)
+        {
+            if (a.Height != b.Height || a.Width != b.Width)
+                throw new ArgumentException("Матриці повинні мати однакові розміри для додавання.");
+
+            var result = new double[a.Height, a.Width];
+            for (int i = 0; i < a.Height; i++)
+            {
+                for (int j = 0; j < a.Width; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return new MyMatrix(result);
+        }
+
+        public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+        {
+            if (a.Width != b.Height)
+                throw new ArgumentException("Множення матриць не визначено для заданих розмірів.");
+
+            var result = new double[a.Height, b.Width];
+            for (int i = 0; i < a.Height; i++)
+            {
+                for (int j = 0; j < b.Width; j++)
+                {
+                    for (int k = 0; k < a.Width; k++)
+                    {
+                        result[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return new MyMatrix(result);
+        }
+
+        private double[,] GetTransponedArray()
+        {
+            var transposed = new double[Width, Height];
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    transposed[j, i] = elements[i, j];
+                }
+            }
+            return transposed;
+        }
+
+        public MyMatrix GetTransponedCopy()
+        {
+            return new MyMatrix(GetTransponedArray());
+        }
+
+        public void TransponeMe()
+        {
+            elements = GetTransponedArray();
+        }
     }
 }
